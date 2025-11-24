2c5bf1b934273f3dd38fb2ef8453e9c6d92096ae
PromitDutta1 <promitduttaoffice@gmail.com>
2025-11-24 06:36:20 +0530

    Add Firebase configuration and initialization

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB_Xz-MtXJD5nHfElP5MmhN2T6iNJlYMSk",
+  authDomain: "procheeser-824bf.firebaseapp.com",
+  projectId: "procheeser-824bf",
+  storageBucket: "procheeser-824bf.firebasestorage.app",
+  messagingSenderId: "1073557932074",
+  appId: "1:1073557932074:web:6ba905d0b6caeee61d9b53",
+  measurementId: "G-D94BYJCPDW",
+  databaseURL: "https://procheeser-824bf-default-rtdb.firebaseio.com/"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
